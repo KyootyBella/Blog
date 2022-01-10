e1a9344c083a7ee98d3fa32f6bfcf71a9e8dc32d
KyootyBella <sebastian10ask@gmail.com>
2022-01-11 00:03:53 +0100

    Made responsive for Ipads

--- scripts/index.js
+++ scripts/index.js
@@ -48,13 +48,3 @@ function myFunction() {
     x.className = "navbar";
   }
 }
-
-// Get the modal
-var modal = document.getElementById("loginpopup");
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
